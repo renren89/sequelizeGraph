bead88552667468f31b049b365d385a1e5ce58b5
renren89 <RenHe@Rens-iMac.local>
2016-01-17 18:29:40 -0800

    added boilerplate for redux

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-render(<App />, document.getElementById('root'));
+const reducers = {
+  form: formReducer
+};
+const reducer = combineReducers(reducers);
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const store = createStoreWithMiddleware(reducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
