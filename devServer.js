801971e52eb450ba9478c4c91f336545e19e1f29
renren89 <guishenhe@gmail.com>
2016-04-06 06:25:51 -0700

    optional ports

--- devServer.js
+++ devServer.js
@@ -3,11 +3,12 @@ import express from 'express';
 import webpack from 'webpack';
 import config from './webpack.config.dev';
 import bodyParser from 'body-parser';
-import cassandra from 'cassandra-driver';
+// import cassandra from 'cassandra-driver';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddlware from 'webpack-hot-middleware';
 
-const client = new cassandra.Client({ contactPoints: ['127.0.0.1']});
+const port = process.argv[2] || 3000;
+// const client = new cassandra.Client({ contactPoints: ['127.0.0.1']});
 const app = express();
 const compiler = webpack(config);
 
@@ -17,23 +18,23 @@ app.use((webpackMiddleware)(compiler, {
 }));
 app.use((webpackHotMiddlware)(compiler));
 app.use(bodyParser.json());
-client.connect((err, res) => {
-  if (err) {
-    let error = new Error('could not connect to Cassandra Node');
-    console.log(error);
-  } else {
-    console.log('Successfully connected to Cassandra')
-  }
-});
+// client.connect((err, res) => {
+//   if (err) {
+//     let error = new Error('could not connect to Cassandra Node');
+//     console.log(error);
+//   } else {
+//     console.log('Successfully connected to Cassandra')
+//   }
+// });
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, 'localhost', err => {
+app.listen(port, 'localhost', err => {
   if (err) {
     console.log(err);
     return;
   }
-  console.log('Listening at http://localhost:3000');
+  console.log(`Listening at http://localhost:${port}`);
 });
